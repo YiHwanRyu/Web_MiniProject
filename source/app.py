dad4a7add6888a8ebc429fe6f90952df6decd87a
YiHwanRyu <yihwandev@gmail.com>
2023-06-08 08:54:01 +0900

    [UDDATE] 기본 템플릿, API 설정

--- source/app.py
+++ source/app.py
@@ -1,6 +1,6 @@
 # commit과 push하시기 전에 꼭 branch확인 부탁드립니다!
 # main에 하시면 안됩니다!
-# page이동 기능은 구현하고 말씀드릴게요 먼저 홈에서 기본적으로 동작한다고 생각하시면 됩니다. 이후에 경로만 수정할게요.
+
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
@@ -12,20 +12,25 @@ client = MongoClient('mongodb+srv://   /?retryWrites=true&w=majority')
 db = client.dbsparta
 
 
-# 팬명록 작성코드의 일부를 남겨두었습니다.
 @app.route('/')
-def home(): 
-   return render_template('index.html') # 시작시 기본 코드
+def index(): 
+   return render_template('index.html') 
 
-@app.route("/home", methods=["POST"])
-def profile_list():
-    return jsonify({'msg': '저장 완료!'})
+@app.route('/profile', methods=["POST"])
+def profile_create():
+    return
 
-@app.route("/guestbook", methods=["GET"])
-def profile_get():
-    all_fans = list(db.guestbook.find({},{'_id':False}))
-    return jsonify({'result': all_fans})
+@app.route('/profile/list')
+def profile_read():
+    return
 
+@app.route('/profile/update', method=["POST"])
+def profile_update():
+    return
+
+@app.route('/profile/delete', method=["POST"])
+def profile_delete():
+    return
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
