997d675edfbfdf057db1c6db2055c8ad7ab11011
YiHwanRyu <yihwandev@gmail.com>
2023-06-08 15:49:25 +0900

    [UPDATE] 1차 수정

--- source/app.py
+++ source/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@cluster0.s6qwrpu.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv:')
 db = client.dbsparta
 
 @app.route('/')
@@ -40,10 +40,34 @@ def profile_read():
     all_profiles = list(db.web.find({},{'_id':False}))
     return jsonify({'result':all_profiles})
 
+@app.route('/profile/updatehtml', methods=["POST"])
+def updatehtml():
+    profile_update = request.form['profile_update'] 
+    return render_template('update.html', number=profile_update)
+
 
 @app.route('/profile/update', methods=["POST"]) #프로필 수정
 def profile_update():
-    return
+    number_receive = request.form["number_give"]
+    name_receive = request.form["name_give"]
+    age_receive = request.form["age_give"]
+    hobby_receive = request.form["hobby_give"]
+    mbti_receive = request.form["mbti_give"]
+    goal_receive = request.form["goal_give"]
+    food_receive = request.form["food_give"]
+
+    db.web.update_one({'number':number_receive},
+    {'$set':
+    {
+        'name':name_receive,
+        'age':age_receive,
+        'hobby':hobby_receive,
+        'mbti':mbti_receive,
+        'goal':goal_receive,
+        'food':food_receive
+    }
+    })
+    return jsonify({'msg': "수정 완료!"})
 
 @app.route('/profile/delete', methods=["DELETE"]) #프로필 삭제
 def profile_delete():
