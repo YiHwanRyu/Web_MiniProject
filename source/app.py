1fe4b4af770b429fd678cc577f9242142eddc58e
yoonji8 <dbswl4342@gmail.com>
2023-06-08 11:20:39 +0900

    d
    d

--- source/app.py
+++ source/app.py
@@ -1,34 +1,51 @@
-# commit과 push하시기 전에 꼭 branch확인 부탁드립니다!
-# main에 하시면 안됩니다!
-
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
-# DB주소는 일단 각자 DB로 작성하셔서 확인하면서 진행해주시면 됩니다.
-# 추후에 DB통합 할게요!
-# 아래 빈 공백
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://   /?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://test:sparta@cluster0.s6qwrpu.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
-
 
 @app.route('/')
 def index(): 
    return render_template('index.html') 
 
-@app.route('/profile', methods=["POST"])
+@app.route('/profile', methods=["POST"]) #create 프로필 생성
 def profile_create():
-    return
+    name_receive = request.form["name_give"]
+    age_receive = request.form["age_give"]
+    hobby_receive = request.form["hobby_give"]
+    mbti_receive = request.form["mbti_give"]
+    goal_receive = request.form["goal_give"]
+    food_receive = request.form["food_give"]
 
-@app.route('/profile/list')
+    profile_list = list(db.web.find({}, {'_id': False}))
+    count = len(profile_list) + 1
+
+    doc = {
+        'number':count,
+        'name':name_receive,
+        'age':age_receive,
+        'hobby':hobby_receive,
+        'mbti':mbti_receive,
+        'goal':goal_receive,
+        'food':food_receive,
+        'done':0,
+    }
+
+    db.web.insert_one(doc)
+    return jsonify({'msg':'등록 완료!'})
+
+@app.route('/profile', methods=["GET"]) #프로필 조회
 def profile_read():
-    return
+    all_profiles = list(db.web.find({},{'_id':False}))
+    return jsonify({'result':all_profiles})
 
-@app.route('/profile/update', method=["POST"])
+
+@app.route('/profile/update', methods=["POST"]) #프로필 수정
 def profile_update():
     return
 
-@app.route('/profile/delete', method=["POST"])
+@app.route('/profile/delete', methods=["POST"]) #프로필 삭제
 def profile_delete():
     return
 
